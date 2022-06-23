325bcb5cea5fe926d07a3503c5ae9fe6d594e5fc
bassem1806 <bendhief.b@gmail.com>
2022-06-23 12:07:54 +0100

    add template dashbord

---
+++ src/main/java/com/example/Securite_Routiere/controller/DashboardController.java
@@ -0,0 +1,29 @@
+package com.example.Securite_Routiere.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DashboardController {
+	
+	@RequestMapping(value = "/dashboard")
+    public ModelAndView dashboard() {
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String date_time = dtf.format(now);
+
+        Map params = new HashMap<String, Object>();
+        params.put("date_time", date_time);
+
+        return new ModelAndView("dashboard", params);
+    }
+
+}
