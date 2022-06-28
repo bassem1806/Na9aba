6f78cb25838d9129c18725c593dea08da4f1e9d6
bassem1806 <bendhief.b@gmail.com>
2022-06-28 10:19:02 +0100

    add role

---
+++ src/main/java/com/example/Securite_Routiere/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.Securite_Routiere.repositories;
+
+
+import com.example.Securite_Routiere.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    User findByEmail(String email);
+}
